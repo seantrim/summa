1e4464424bf86429054998ce5fa4e7c61612f7e8
seantrim <strim201325@gmail.com>
2025-09-24 06:38:43 -0600

    Added operations for solve_with_ARKODE in systemSolv.

--- build/source/engine/systemSolv.f90
+++ build/source/engine/systemSolv.f90
@@ -639,12 +639,133 @@ contains
   ! contains initialize-update-finalize sequence for ARKODE
   ! note: may be able to use solve_with_IDA internal subroutine as a guide for development
 #ifdef SUNDIALS_ACTIVE
-  ! [...] initialize operations here
+  ! ** initialize operations **
 
-  ! update operations
+  ! get tolerance vectors
+  call popTol4ida(&
+                  ! input
+                  nState,        & ! intent(in):  number of desired state variables
+                  prog_data,     & ! intent(in):  model prognostic variables for a local HRU
+                  diag_data,     & ! intent(in):  model diagnostic variables for a local HRU
+                  indx_data,     & ! intent(in):  indices defining model states and layers
+                  mpar_data,     & ! intent(in):  model parameters
+                  ! output
+                  atol,          & ! intent(out): absolute tolerances vector (mixed units)
+                  rtol,          & ! intent(out): relative tolerances vector (mixed units)
+                  err,cmessage)    ! intent(out): error control
+  if (err/=0) then; message=trim(message)//trim(cmessage); return_flag=.true.; return; end if  ! check for errors
+
+  associate(&
+   nSnow => indx_data%var(iLookINDEX%nSnow)%dat(1),& ! intent(in): [i4b] number of snow layers
+   nSoil => indx_data%var(iLookINDEX%nSoil)%dat(1) & ! intent(in): [i4b] number of soil layers
+  &)
+
+   ! allocate space for the temporary flux_sum structure
+   call allocLocal(flux_meta(:),flux_sum,nSnow,nSoil,err,cmessage)
+   if (err/=0) then; err=20; message=trim(message)//trim(cmessage); return_flag=.true.; return; end if
+
+   ! initialize flux_sum
+   do concurrent ( iVar=1:size(flux_meta) )
+    flux_sum%var(iVar)%dat(:) = 0._rkind
+   end do
+   ! initialize sum of compression of the soil matrix
+   mLayerCmpress_sum(:) = 0._rkind
+   stateVecNew(:) = 0._rkind 
+   !stateVecPrime(:) = 0._rkind
+
+  end associate
+
+  ! ** update operations **
   ! call summaSolve4arkode(...) 
 
-  ! [...] finalize operations here
+   !---------------------------
+   ! * solve My' = fE(t,y) + fI(t,y) using ARKODE (y is the state vector, y'=dy/dt) *
+   !---------------------------
+  associate(&
+   nSnow => indx_data%var(iLookINDEX%nSnow)%dat(1),& ! intent(in): [i4b] number of snow layers
+   nSoil => indx_data%var(iLookINDEX%nSoil)%dat(1) & ! intent(in): [i4b] number of soil layers
+  &)
+   ! iterations and updates to trial state vector, fluxes, and derivatives are done inside ARKODE solver
+   call summaSolve4arkode(&
+                          dt_cur,                  & ! intent(in):    current stepsize
+                          dt,                      & ! intent(in):    entire time step for drainage pond rate
+                          atol,                    & ! intent(in):    absolute tolerance
+                          rtol,                    & ! intent(in):    relative tolerance
+                          nSnow,                   & ! intent(in):    number of snow layers
+                          nSoil,                   & ! intent(in):    number of soil layers
+                          nLayers,                 & ! intent(in):    number of snow+soil layers
+                          nState,                  & ! intent(in):    number of state variables in the current subset
+                          ixMatrix,                & ! intent(in):    type of matrix (dense or banded)
+                          firstSubStep,            & ! intent(in):    flag to indicate if we are processing the first sub-step
+                          computeVegFlux,          & ! intent(in):    flag to indicate if we need to compute fluxes over vegetation
+                          scalarSolution,          & ! intent(in):    flag to indicate the scalar solution
+                          computMassBalance,       & ! intent(in):    flag to compute mass balance
+                          computNrgBalance,        & ! intent(in):    flag to compute energy balance
+                          ! input: state vector
+                          stateVecTrial,           & ! intent(in):    model state vector at the beginning of the data time step
+                          sMul,                    & ! intent(inout): state vector multiplier (used in the residual calculations)
+                          dMat,                    & ! intent(inout): diagonal of the Jacobian matrix (excludes fluxes)
+                          ! input: data structures
+                          model_decisions,         & ! intent(in):    model decisions
+                          lookup_data,             & ! intent(in):    lookup data
+                          type_data,               & ! intent(in):    type of vegetation and soil
+                          attr_data,               & ! intent(in):    spatial attributes
+                          mpar_data,               & ! intent(in):    model parameters
+                          forc_data,               & ! intent(in):    model forcing data
+                          bvar_data,               & ! intent(in):    average model variables for the entire basin
+                          prog_data,               & ! intent(in):    model prognostic variables for a local HRU
+                          ! input-output: data structures
+                          indx_data,               & ! intent(inout): index data
+                          diag_data,               & ! intent(inout): model diagnostic variables for a local HRU
+                          flux_temp,               & ! intent(inout): model fluxes for a local HRU
+                          flux_sum,                & ! intent(inout): sum of fluxes model fluxes for a local HRU over a data step
+                          deriv_data,              & ! intent(inout): derivatives in model fluxes w.r.t. relevant state variables
+                          mLayerCmpress_sum,       & ! intent(inout): sum of compression of the soil matrix
+                          ! output
+                          ixSaturation,            & ! intent(inout): index of the lowest saturated layer (NOTE: only computed on the first iteration)
+                          sunSucceeds,             & ! intent(out):   flag to indicate if ida successfully solved the problem in current data step
+                          tooMuchMelt,             & ! intent(inout): flag to denote that there was too much melt
+                          nSteps,                  & ! intent(out):   number of time steps taken in solver
+                          stateVecNew,             & ! intent(inout): model state vector (y) at the end of the data time step
+                          !stateVecPrime,           & ! intent(inout): derivative of model state vector (y') at the end of the data time step
+                          balance,                 & ! intent(inout): balance per state
+                          err,cmessage)              ! intent(out):   error control
+  end associate
+
+  ! ** finalize operations **
+
+  ! check if ARKODE is successful, only fail outright in the case of a non-recoverable error
+  if ( .not.sunSucceeds ) then
+   message=trim(message)//trim(cmessage)
+   !if (err.ne.-20 .or. err=0) err = 20 ! 0 if infeasible solution, could happen since not using imposeConstraints 
+   if (err.ne.-20) err = 20 ! -20 is a recoverable error
+   return_flag=.true.; return
+  else
+   if (tooMuchMelt) then
+    return_flag=.true.; return ! exit to start same step over after merge
+   end if
+  end if
+  niter = 0  ! iterations are counted inside IDA solver
+
+  ! save the computed solution
+  stateVecTrial = stateVecNew
+
+  ! compute average flux
+  do iVar=1,size(flux_meta)
+   flux_temp%var(iVar)%dat(:) = ( flux_sum%var(iVar)%dat(:) ) / dt_cur
+  end do
+
+  ! compute the total change in storage associated with compression of the soil matrix (kg m-2)
+  associate(&
+   nSnow              => indx_data%var(iLookINDEX%nSnow)%dat(1)            ,& ! number of snow layers
+   nSoil              => indx_data%var(iLookINDEX%nSoil)%dat(1)            ,& ! number of soil layers
+   mLayerDepth        => prog_data%var(iLookPROG%mLayerDepth)%dat          ,& ! depth of each layer in the snow-soil sub-domain (m)
+   mLayerCompress     => diag_data%var(iLookDIAG%mLayerCompress)%dat       ,& ! change in storage associated with compression of the soil matrix (-)
+   scalarSoilCompress => diag_data%var(iLookDIAG%scalarSoilCompress)%dat(1) & ! total change in storage associated with compression of the soil matrix (kg m-2 s-1)
+  &)
+   mLayerCompress = mLayerCmpress_sum / dt_cur
+   scalarSoilCompress = sum(mLayerCompress(1:nSoil)*mLayerDepth(nSnow+1:nLayers))*iden_water
+  end associate
 #endif
  end subroutine solve_with_ARKODE
 
